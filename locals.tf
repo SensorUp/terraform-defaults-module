77c7f4ed794c6d8b288066eccb143cbf243dceee
will Farrell <will.farrell@gmail.com>
2019-06-16 15:17:48 -0600

    feat: upgrade to v0.12

--- locals.tf
+++ locals.tf
@@ -1,19 +1,26 @@
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
+data "aws_region" "current" {
+}
+
+data "aws_caller_identity" "current" {
+}
 
 locals {
-  account_id        = "${data.aws_caller_identity.current.account_id}"
-  profile           = "${data.aws_caller_identity.current.user_id}"
-  region            = "${data.aws_region.current.name}"
-  name              = "${replace(var.name, "/[^a-zA-Z0-9-]/", "-")}"
-  name_alphanumeric = "${replace(var.name, "/[^a-zA-Z0-9]/", "")}"
+  account_id        = data.aws_caller_identity.current.account_id
+  profile           = data.aws_caller_identity.current.user_id
+  region            = data.aws_region.current.name
+  name              = replace(var.name, "/[^a-zA-Z0-9-]/", "-")
+  name_alphanumeric = replace(var.name, "/[^a-zA-Z0-9]/", "")
 
   # Sanitize name, waf labels follow different rules
 
-  tags = "${merge(map(
-    "Terraform", "true",
-    "Environment", "${terraform.workspace}",
-    "Name","${replace(var.name, "/[^a-zA-Z0-9-]/", "-")}",
-    "Description",""
-  ), var.tags)}"
+  tags = merge(
+    {
+      "Terraform"   = "true"
+      "Environment" = terraform.workspace
+      "Name"        = replace(var.name, "/[^a-zA-Z0-9-]/", "-")
+      "Description" = ""
+    },
+    var.tags,
+  )
 }
+
--- output.tf
+++ output.tf
@@ -1,19 +1,20 @@
 output "account_id" {
-  value = "${local.account_id}"
+  value = local.account_id
 }
 
 output "profile" {
-  value = "${local.profile}"
+  value = local.profile
 }
 
 output "region" {
-  value = "${local.region}"
+  value = local.region
 }
 
 output "name" {
-  value = "${local.name}"
+  value = local.name
 }
 
 output "tags" {
-  value = "${local.tags}"
+  value = local.tags
 }
+
